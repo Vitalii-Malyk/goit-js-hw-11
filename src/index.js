ddba58af892ffdaa14cc8e285341e1ff5fa1cf85
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2023-08-02 21:23:00 +0200

    стилі та початок коду

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-const refs = document.querySelector('#search-form');
+const formEl = document.querySelector('#search-form');
+
+formEl.addEventListener('Submit', onSubmit);
+
+function onSubmit() {
+  console.log(formEl.element.searchQuery.value);
+}
+
+const options = {
+  key: 555,
+  q: 222,
+};
